60510763c806450c670469e57327d759500b0ab9
福島恵珠 <nesskindbead2@gmail.com>
2023-02-06 20:15:50 +0900

    feat: react-tsparticlesの仮実装

---
+++ pages/top.tsx
@@ -0,0 +1,121 @@
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const Top = () => {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        console.log(engine);
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#000",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "bubble",
+                        },
+                        resize: true,
+                    },
+                    // modes: {
+                    //     push: {
+                    //         quantity: 4,
+                    //     },
+                    //     repulse: {
+                    //         distance: 200,
+                    //         duration: 0.4,
+                    //     },
+                    // },
+                    modes: {
+                        grab: {
+                            distance: 400,
+                            line_linked: {
+                            opacity: 1
+                        }
+                        },
+                        bubble: {
+                        distance: 400,
+                            size: 40,
+                            duration: 2,
+                            opacity: 0.45554445554445555,
+                            speed: 3
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4
+                        },
+                        push: {
+                            particles_nb: 4
+                        },
+                        remove: {
+                            particles_nb: 2
+                        }
+                    }
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    );
+};
+
+export default Top
