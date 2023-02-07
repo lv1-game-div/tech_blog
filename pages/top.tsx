dce2b1f834f3cfa5e1178bf61a0d846303cac316
福島恵珠 <nesskindbead2@gmail.com>
2023-02-07 20:43:24 +0900

    fix: 描画処理の軽量化

--- pages/top.tsx
+++ pages/top.tsx
@@ -27,11 +27,11 @@ const Top = () => {
                 interactivity: {
                     events: {
                         onClick: {
-                            enable: true,
+                            enable: false,
                             mode: "push",
                         },
                         onHover: {
-                            enable: true,
+                            enable: false,
                             mode: "bubble",
                         },
                         resize: true,
