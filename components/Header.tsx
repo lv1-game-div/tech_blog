9dffd841b7bcb1a65e76e4247d5b038955913e5e
福島恵珠 <nesskindbead2@gmail.com>
2023-03-21 13:39:20 +0900

    style: headerの改修

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,29 @@
-import Link from 'next/link'
+import { ImTwitter, ImGithub } from "react-icons/im";
+import Link from "next/link";
 
 export default function Header() {
-  return (
-    <header className="text-gray-600 body-font bg-black">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link legacyBehavior href={'/'} passHref>
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-xl text-white">Tech Blog</span>
-          </a>
-        </Link>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 text-white hover:text-gray-900">Profile</a>
-        </nav>
-      </div>
-    </header>
-  )
-}
+    return (
+        <header className="bg-slate-900">
+            <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
+                <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
+                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+                </div>
+                <div className="shrink w-80 sm:order-2">
+                    <Link href={"/"}>
+                        <span className="text-white font-bold uppercase text-3xl">Tech Blog</span>
+                    </Link>
+                </div>
+                <div className="w-96 order-3 flex justify-center">
+                    <div className="flex gap-6">
+                        <Link href="https://twitter.com/devkei5" target="_blank" rel="noopener noreferrer">
+                            <ImTwitter color="#888888" />
+                        </Link>
+                        <Link href="https://github.com/lv1-game-div" target="_blank" rel="noopener noreferrer">
+                            <ImGithub color="#888888" />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
